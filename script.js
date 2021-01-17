e47f2b25b3e7410f2f13060888c40d828bbd57e1
Balázs Szeidl <szeidl.balazs@hotmail.com>
2021-01-17 21:01:51 +0100

    share

---
+++ script.js
@@ -0,0 +1,77 @@
+function pageLoad() {
+
+		let _bodyId = document.querySelector('#bodyId');
+		/*Listen the change of rgb values in the sepcific input field for r, g, b values:*/
+		const inputR = document.querySelector('#red');			/*store input tag rgb R in inputR variable*/
+	 	const inputG = document.querySelector('#green');
+		const inputB = document.querySelector('#blue');
+		const _transp = document.querySelector('#transp');
+	 	const rText = document.querySelector('#rText');			/*store the output tag to display rgb red value, in this case this is a <p> tag */
+	 	const gText = document.querySelector('#gText');
+	 	const bText = document.querySelector('#bText');
+
+	 	let rgbToHex = function (r, g, b) { 
+		let hexR = Number(r).toString(16);									/*hexadecimal conversion from type of number to string*/
+		let hexG = Number(g).toString(16);
+		let hexB = Number(b).toString(16);
+		
+		if (hexR.length < 2) {
+			hexR = "0" + hexR;
+		}	
+
+		if (hexG.length < 2) {
+			hexG = "0" + hexG;
+		}	
+		 
+		if (hexB.length < 2) {
+			hexB = "0" + hexB;
+		}	
+		let hexVal = "#" + hexR + hexG + hexB;								/*store the hex components in hexVal*/
+		
+		_bodyId.style.backgroundColor = hexVal;								/*changes the background color with HEX STRING value!!!*/
+		
+		return "Hex value: #" + hexR + hexG + hexB;						/*displays the hex value*/
+	}
+	
+
+	 	let r, g, b;
+	 	inputR.addEventListener('input', updateRValue);										/*listen the input field of rgb R value and if changes activate updateRValue function*/
+	 	function updateRValue(e) {			
+			rText.innerHTML = `Red: ${Number(e.target.value).toString(2)}`;	/*r value is taken from the eventlistener, updateRValue function accesses the <p> tag (rText) of rgb red value and updates*/
+																																			
+			r = document.getElementById("red").value;
+			g = document.getElementById("green").value;
+			b = document.getElementById("blue").value;
+			rgbToHex(r, g, b);																							/*call rgbToHex function passing a value stored in r variable*/
+			/*rText.innerHTML = Number(r).toString(2);*/										/*This alternative takes r value directly from input filed */
+		}		
+		
+		inputG.addEventListener('input', updateGValue);
+		function updateGValue(e) {
+		gText.innerHTML = `Green: ${Number(e.target.value).toString(2)}`;		/* binary conversion from type of number to string*/
+		r = document.getElementById("red").value;
+		g = document.getElementById("green").value;
+		b = document.getElementById("blue").value;
+		rgbToHex(r, g, b);			
+		}
+
+		
+		inputB.addEventListener('input', updateBValue);
+		function updateBValue(e) {
+			bText.innerHTML = `Blue: ${Number(e.target.value).toString(2)}`;
+			r = document.getElementById("red").value;
+			g = document.getElementById("green").value;
+			b = document.getElementById("blue").value;
+			rgbToHex(r, g, b);											
+		}	
+
+		let tra;
+		_transp.addEventListener('input', updateTransp);			/*watch the value change of transparency input*/
+		function updateTransp(e) {
+			tra = Number(e.target.value) / 100;									/*the function stores the input value in tra. 100 devision because opacity range is 0.0-1.0*/
+			bodyId.style.opacity = tra;													/*the body opacity receives the velue in type of number!!!*/
+		}
+		
+}
+
+window.addEventListener('load', pageLoad);
